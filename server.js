4ad1995fa372ba10a57d46c7ebb619983b576cfb
Glitch (file-sizzer) <none>
2018-01-20 03:12:33 +0000

    👊📉 Updated with Glitch

--- server.js
+++ server.js
@@ -4,34 +4,30 @@
 // init project
 var express = require('express');
 var app = express();
+var multer = require('multer');
+const bodyParser = require('body-parser');
+
+var upload = multer()                    
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.post("/upload",upload.single('upfile'),function(req,res){
+    
+  
+  res.end(JSON.stringify({size:req.file.size}));
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
